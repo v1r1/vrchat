e4584ec890e03aa689a04de090d8c1b887cf40c6
viri.space <admin@viri.xyz>
2019-08-16 16:38:42 +0100

    gitignore actually works now, also default avatar scene files

---
+++ Assets/VIRI.SPACE/VRChat/Editor/SceneControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SCWindow : EditorWindow {
+
+		string myString = "aww man";
+		bool cameraEnabled;
+
+		[MenuItem("VIRI.SPACE/VRChat/Scene Control")]
+		public static void ShowWindow ()
+		{
+				GetWindow<SCWindow>("Scene Control");
+		}
+
+		void OnGUI ()
+		{
+				if (GUILayout.Button("X", GUILayout.Width(30)))
+				{
+					this.Close();
+				}
+				GUILayout.Label("Scene Control", EditorStyles.boldLabel);
+
+				myString = EditorGUILayout.TextField("creeper", myString);
+		}
+}
+
+public class ACWindow : EditorWindow {
+
+		string myString = "avatar name";
+		bool avatarEnabled;
+
+		[MenuItem("VIRI.SPACE/VRChat/Avatar Control")]
+		public static void ShowWindow ()
+		{
+				GetWindow<ACWindow>("Avatar Control");
+		}
+
+		void OnGUI ()
+		{
+				if (GUILayout.Button("X", GUILayout.Width(30)))
+				{
+					this.Close();
+				}
+
+				GUILayout.Label("Avatar Control", EditorStyles.boldLabel);
+				myString = EditorGUILayout.TextField("Name", myString);
+    }
+}
